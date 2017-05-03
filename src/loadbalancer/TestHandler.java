6e87f18d5d8bfe8d94e8c12be168a55fdca750f0
João Barbosa <bmpj13@hotmail.com>
2017-05-04 00:37:30 +0100

    fixed query redirect; needs code cleanup

--- src/loadbalancer/TestHandler.java
+++ src/loadbalancer/TestHandler.java
@@ -10,7 +10,14 @@ import utils.Exchanges;
 public class TestHandler implements HttpHandler {
     @Override
     public void handle(HttpExchange httpExchange) throws IOException {
-        Exchanges.redirectTo(httpExchange, "http://localhost:7001/test");
+    	StringBuilder builder = new StringBuilder();
+    	
+    	builder.append("http://localhost:7001/test");
+    	if (httpExchange.getRequestURI().getQuery() != null) {
+    		builder.append("?" + httpExchange.getRequestURI().getQuery());
+    	}
+    	
+        Exchanges.redirectTo(httpExchange, builder.toString());
         httpExchange.close();
     }
 }
